b4a8febd06fbdecd0684143ab3543e918ccd89e0
Hoang Nguyen <hoang.nguyen2@niteco.se>
2021-06-14 15:37:47 +0700

    fix config

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-API_KEY = "Ql85r6kEDi0NPwxjb6uvYl4xScIa0tPvicuhKlouZnSM5MbSloXAVNiZL5rm401M"
-API_SECRET = "GeoS3qLAJw9Fb0kuNPmmAtqotuTAbNX1caWyR88f7SOMqNZlxPEJuq1Nu8mhZrCd"
+API_KEY = ""
+API_SECRET = ""
